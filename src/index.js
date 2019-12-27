53005fe2ac577799f22bd9dfcd5ea2999a34987a
Jack Frolov <jackdev415@gmail.com>
2019-12-27 15:55:24 -0800

    set ESlint AirBnB

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Root(){
-    return <h1>Hello, world.</h1>;
+function Root() {
+  return <h1>Hello, world.</h1>;
 }
 
 ReactDOM.render(
-    <Root />,
-    document.getElementById('root')
-)
+  <Root />,
+  document.getElementById('root'),
+);
