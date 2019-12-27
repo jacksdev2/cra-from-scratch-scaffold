247fa1b00dec22cfe90732b50406c80d3a1bb9d2
Jack Frolov <jackdev415@gmail.com>
2019-12-26 16:44:40 -0800

    added webpack

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
+import 'core-js/stable';
+import sum from './sum';
 
 const greet = name => console.log(` Hello, ${name} `)
+
+console.log(sum(8,4))
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const PATH_SOURCE = path.join(__dirname, './src');
+const PATH_DIST = path.join(__dirname, './dist');
+
+module.exports = env => {
+
+    const environment = env.environment;
+    const isProduction = environment === 'production';
+    const isDevelopment = environment === 'development';
+
+    return {
+        mode: environment,
+        entry: [
+            path.join(PATH_SOURCE, './index.js')
+        ],
+        output: {
+            path: PATH_DIST,
+            filename: 'js/[name].[hash].js'
+        },
+    
+        module:{
+            rules:[
+                {
+                    test:/\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options:{
+                            presets:[
+                                [
+                                    '@babel/preset-env',
+                                    {
+                                        debug: true,
+                                        useBuiltIns: 'usage',
+                                        corejs: 3,
+                                    }
+                                ]
+                            ]
+                        }
+                    }
+                }
+            ]
+        },
+
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: path.join(PATH_SOURCE, './index.html'),
+            }),
+
+            new CleanWebpackPlugin(),
+        ],
+    }
+// module end
+}
+
+
+
+
+
+
+
